102b8e5e8186350eae831aa1988da646a51bc0e6
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-11 04:01:28 -0800

    add

---
+++ data_13.r
@@ -0,0 +1,15 @@
+library(dplyr)
+library(corrplot)
+chocolates2 <- chocolates %>% 
+  select(atribut_sensoris) %>% 
+  cor() %>% 
+  corrplot(
+    type = "upper",
+    method = "square",
+    diag = FALSE,
+    addgrid.col = FALSE,
+    order = "FPC", 
+    tl.col = "gray30", 
+    tl.srt = 30
+  )
+chocolates2
